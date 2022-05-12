2642ffb62fbf544fd351b1b3a1d5528821214a37
tandiep48 <68583489+tandiep48@users.noreply.github.com>
2022-05-13 00:00:19 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mysql = require('mysql2');
 var router = express.Router();
 
-const conn = mysql.createPool({
+const conn = mysql.createConnection({
   host: "eu-cdbr-west-02.cleardb.net",
   user: "b8b9ab2a962b71",
   password: "286b5614",
